78e54cd2e679e9fcbf8a75337da4015f77711dcb
l22550601-cyber <l22550601@gmail.com>
2026-04-06 15:36:09 +0000

    ajouts des modifications

--- assets/index-DaBAy5zF.js
+++ assets/index-DaBAy5zF.js
@@ -18971,7 +18971,7 @@ function Cu() {
             S.jsxs)(`a`, {
                 href: `#hero`,
                 className: `font-syne text-2xl font-bold tracking-tight text-blanc-casse`,
-                children: [`Kabir`, (0,
+                children: [`Abdoul-Kabir`, (0,
                 S.jsx)(`span`, {
                     className: `text-rouge`,
                     children: `.`
